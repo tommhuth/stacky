7b9ee4416f95ecde144f5af42d3bf69af43baaee
Tomm Huth <tomm.huth@makingwaves.com>
2017-11-21 09:49:30 +0100

    light update

--- src/world/scene.js
+++ src/world/scene.js
@@ -1,4 +1,4 @@
-import { Engine, FreeCamera, Animation, Scene, HemisphericLight, Vector3, Camera, Color3, Color4, PointLight, SineEase, EasingFunction } from "babylonjs"
+import { Engine, FreeCamera, Animation, Scene, DirectionalLight, Vector3, Camera, Color3, Color4, SineEase, EasingFunction } from "babylonjs"
 import uuid from "uuid/v1"
 
 const frustumSize = 100
@@ -8,8 +8,7 @@ const canvas = document.getElementById("app")
 const engine = new Engine(canvas, true, undefined, true)
 const scene = new Scene(engine)
 const camera = new FreeCamera(uuid(), new Vector3(-45, 45, -45), scene)
-const light = new HemisphericLight(uuid(), new Vector3(0, -1, 0), scene)
-const pointLight = new PointLight(uuid(), new Vector3(-85, -55, -95), scene)
+const light = new DirectionalLight(uuid(), new Vector3(.2, -.81, .5), scene)
 
 scene.enablePhysics()
 scene.clearColor = new Color4(0, 0, 0, 0)
@@ -21,11 +20,7 @@ camera.orthoLeft = frustumSize * aspect / - 2
 camera.orthoRight = frustumSize * aspect / 2
 camera.setTarget(Vector3.Zero())
 
-light.groundColor = Color3.White()
-light.diffuse = new Color3(.5, .5, .5)
-light.intensity = .4;
-
-pointLight.intensity = .4
+light.intensity = .6 ; 
 
 window.addEventListener('resize', () => {
     const aspect = window.innerWidth / window.innerHeight
