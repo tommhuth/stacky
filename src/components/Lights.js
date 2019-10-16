c2b77bfa67f59c38038931767ec1dad6ab662ba4
Tomm Huth <tomm.huth@makingwaves.com>
2019-10-16 20:54:47 +0200

    light tweak + less light

--- src/components/Lights.js
+++ src/components/Lights.js
@@ -1,34 +1,28 @@
-import React from "react"
+import React , { useEffect, useRef}from "react"
 import Config from "../Config"
 import { useStore } from "../data/store"
 
 export default function Lights() {
     const stackSize = useStore(state => state.slices.length)
+    let ref = useRef()
+
+    useEffect(() => { 
+        ref.current.target.updateMatrixWorld()
+    }, [ref.current])
 
     return (
         <>
             <ambientLight
                 color={0xFFFFFF}
-                intensity={.25}
+                intensity={.6}
             />
             <directionalLight
+                ref={ref}
                 color={0xFFFFFF}
                 position={[0, stackSize * Config.SLICE_HEIGHT, 0]}
-                intensity={1.09}
-                target-position={[-6, stackSize * Config.SLICE_HEIGHT - 10, -2]}
-                onUpdate={self => {
-                    self.target.updateMatrixWorld()
-                }}
-            />
-            <directionalLight
-                color={0xFFFFFF}
-                position={[0, stackSize * Config.SLICE_HEIGHT, 0]}
-                intensity={.1}
-                target-position={[-6, stackSize * Config.SLICE_HEIGHT - 10, -2]}
-                onUpdate={self => {
-                    self.target.updateMatrixWorld()
-                }}
-            />
+                intensity={.8}
+                target-position={[-6, stackSize * Config.SLICE_HEIGHT - 10, -2]} 
+            /> 
         </>
     )
 }
