5bc06b7a18ffc6ed95a300051d7f12ecc3ad7b78
Tomm Huth <tomm.huth@makingwaves.com>
2019-10-16 20:38:46 +0200

    top slice flicker fix

--- src/components/TopSlice.js
+++ src/components/TopSlice.js
@@ -2,17 +2,17 @@ import React, { useState, useEffect, useRef } from "react"
 import { useRender } from "react-three-fiber"
 import Config from "../Config"
 import { getPositionWithOffset } from "../utils/helpers"
-import Only from "./Only" 
+import Only from "./Only"
 import ColorMixer from "../utils/ColorMixer"
 import { useStore, api } from "../data/store"
 
-export default function TopSlice() { 
+export default function TopSlice() {
     let state = useStore(state => state.state)
     let prev = useStore(state => state.slices[state.slices.length - 1])
     let slices = useStore(state => state.slices)
     let incrementOffset = useStore(state => state.incrementOffset)
     let [color, setColor] = useState()
-    let ref = useRef() 
+    let ref = useRef()
 
     useRender(() => {
         if (state === Config.STATE_ACTIVE) {
@@ -48,10 +48,7 @@ export default function TopSlice() {
 
     return (
         <Only if={state === Config.STATE_ACTIVE}>
-            <mesh
-                ref={ref}
-                key={slices.length} 
-            >
+            <mesh ref={ref}>
                 <boxBufferGeometry
                     attach="geometry"
                     args={[prev.size[0], Config.SLICE_HEIGHT, prev.size[2]]}
